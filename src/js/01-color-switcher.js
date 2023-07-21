be565aa2c11046c26514eaea9fe0d9f8e921f864
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-22 00:15:02 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector("button[data-start]");
+const resetColorBtn = document.querySelector("button[data-stop]");
+
+changeColorBtn.addEventListener("click", changeBgColor);
+function changeBgColor() { 
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+    changeColorBtn.disabled = true;
+    resetColorBtn.disabled = false;
+}
+
+resetColorBtn.addEventListener("click", resetBgColor);
+function resetBgColor() { 
+changeColorBtn.disabled = false;
+resetColorBtn.disabled = true;}
