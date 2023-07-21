31a1838ebc2b029a195cb2ab2075e1f45388d7a9
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-21 22:15:28 +0000

    Deploying to gh-pages from @ NanaTsK/goit-js-hw-09@be565aa2c11046c26514eaea9fe0d9f8e921f864 🚀

---
+++ 01-color-switcher.d1c7a294.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){var o="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));t.style.backgroundColor=o,e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.d1c7a294.js.map
---
+++ 01-color-switcher.e84efabb.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){const o=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=o,e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.e84efabb.js.map
