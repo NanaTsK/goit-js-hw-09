09581622a7f9daf6ba744e57f9157e8d3f497ae7
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-21 23:57:50 +0000

    Deploying to gh-pages from @ NanaTsK/goit-js-hw-09@3f34eba282a26be8dde473309040f85ac5e0a390 🚀

---
+++ 01-color-switcher.e0cc25f6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),a=null;function o(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){a=setInterval(o,1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e0cc25f6.js.map
