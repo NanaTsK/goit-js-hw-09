30b777df19c7c3a8287e7e6739434448137ee90f
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-22 23:00:01 +0200

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,21 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const calendar = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("button[data-start]");
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const fpCalendar = flatpickr(calendar, options);
+
+// Notify.warning('Memento te hominem esse');
