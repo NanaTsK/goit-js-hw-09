8cc447dce389fa4de726246a663f6571c2c2cc70
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-23 17:53:50 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const promiseForm = document.querySelector(".form");
+promiseForm.addEventListener("submit", onPromiseSubmit);
+
+function onPromiseSubmit(event) { 
+  event.preventDefault();
+
+  let delay = Number(promiseForm.elements.delay.value);
+  let step = Number(promiseForm.elements.step.value);
+  let amount = Number(promiseForm.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) { 
+createPromise(i, delay)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  })
+  .finally(delay += step);
   }
+    event.currentTarget.reset();
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => { 
+
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
